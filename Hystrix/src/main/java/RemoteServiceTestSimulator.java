4653aedc5d189f6f3d37ffb3010b0e3569c0f295
suneelyadava <suneelyadava@gmail.com>
2018-03-29 14:28:24 +0530

    HystrixTestDemo

---
+++ Hystrix/src/main/java/RemoteServiceTestSimulator.java
@@ -0,0 +1,13 @@
+class RemoteServiceTestSimulator {
+ 
+    private long wait;
+ 
+    RemoteServiceTestSimulator(long wait) throws InterruptedException {
+        this.wait = wait;
+    }
+ 
+    String execute() throws InterruptedException {
+        Thread.sleep(wait);
+        return "Success";
+    }
+}
